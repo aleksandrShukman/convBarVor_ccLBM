da40a2d2bbde0a7eb10fcba7535b62e0f15fcb82
aleksandrShukman <aleksandrshukman@protonmail.com>
2025-10-25 18:37:22 +0200

    Initial Commit

---
+++ convBarVor_ccLBM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  convBarVor_ccLBM
+//
+//  Created by Alexander Schukmann on 25.10.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
